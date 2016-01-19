65abea683998051cc41a6043d6b835d38eb2d82f
Enrico Rubboli <rubboli@gmail.com>
2016-01-19 14:26:43 +0100

    implement db caching

--- workers/stocks.js
+++ workers/stocks.js
@@ -14,9 +14,11 @@ var urlFor= function(symbol) {
 }
 
 // Retrieve only the most recent price from Yahoo
-var getPricesFromYahoo = function(symbol,out) {
-    csv = '';
-    line = 0;
+var getPriceFromYahoo = function(symbol,onSuccess,onError) {
+    var csv = ''
+      , line = 0
+      , closePrice; 
+
     http.get(urlFor(symbol), function(res) {
         res.on('data', function(chunk) {
             if(line == 2){
@@ -24,20 +26,66 @@ var getPricesFromYahoo = function(symbol,out) {
             }
             line += 1;
         }).on('end', function() {
-            // Date,Open,High,Low,Close,Volume,Adj Close
-            var closePrice = csv.split(',')[4];
-            out.write(closePrice);
+            //CSV: Date,Open,High,Low,Close,Volume,Adj Close
+            closePrice = csv.split(',')[4];
+            onSuccess(closePrice);
+        }).on('error', function(e){
+            onError(e)
+        });
+    });
+}
+
+var savePriceToDB = function(symbol,price){
+    db.run("INSERT INTO prices (symbol,price,date) VALUES (?,?,CURRENT_DATE)", [symbol,price]);
+}
+
+var getPriceFromDB = function(symbol, onSuccess, onError) {
+    db.serialize(function(){
+        ensureDBStructure(); 
+        db.get("SELECT price FROM prices WHERE symbol=? AND date=CURRENT_DATE",[symbol], function(err,row){
+            if(row !== undefined){
+                onSuccess(row.price);
+            }else{
+                onError(err);
+            } 
+        });
+    });
+}
+
+var ensureDBStructure = function() {
+    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='prices'", function(error, row) {
+        if(row == undefined){
+            console.log("Creating prices table")
+            db.run("CREATE TABLE prices (symbol TEXT, date TEXT, price NUM);");
+        }
+    });
+}
+
+var getStockPrice = function(symbol, out) {
+    console.log("getting price for "+symbol);
+    getPriceFromDB(symbol, function(price) { //success DB
+        console.log("Retrive price from DB");
+        out.write(price);
+        out.end('');
+    }, function(){ // fail DB
+        console.log("Error Retrieving today's price from DB")
+        getPriceFromYahoo(symbol, function(price) {//success Yahoo
+            savePriceToDB(symbol,price);
+            out.write(price);
+            out.end('');
+        }, function(e) { // Fail Yahoo
+            console.log("Error retriving price from Yahoo: "+e);
             out.end('');
         });
     });
 }
 
-exports.run = function(conn){
+exports.run = function(conn) {
     var stocksWorker = new pigato.Worker(conn, 'stockPrices');
     stocksWorker.start();
 
     stocksWorker.on('request', function(data,reply) {
-        getPricesFromYahoo(data.symbol, reply);
+       getStockPrice(data.symbol,reply);
     }).on('error', function(e) {
         console.log("worker error: " + e.message); 
     });
